fd0bab27f373882e5ae710a1a9aa37db615582e0
Bharati <bharatisharada@gmail.com>
2021-05-22 18:22:55 +0530

    Added comments

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
+// Form submit button element
 const button = document.getElementById('button');
+
+// Form group div element
 const formGroups = document.querySelectorAll('.form-group');
 
+
+// All the input fields in the form
 const name = document.getElementById('name');
 const uname = document.getElementById('uname');
 const email = document.getElementById('email');
 const psd = document.getElementById('psd');
 const retypePsd = document.getElementById('retype-psd');
 
-const nameAlert = document.querySelector('.name-alert');
-const unameAlert = document.querySelector('.uname-alert');
-const emailAlert = document.querySelector('.email-alert');
-const psdAlert = document.querySelector('.psd-alert');
-const retypePsdAlert = document.querySelector('.retype-psd-alert');
 
-const nameTooltip = document.querySelector('.name-tooltip');
-const unameTooltip = document.querySelector('.uname-tooltip');
-const emailTooltip = document.querySelector('.email-tooltip');
-const psdTooltip = document.querySelector('.psd-tooltip');
-const retypePsdTooltip = document.querySelector('.retype-psd-tooltip');
-
-
-
+// Event listener for button click
 button.addEventListener('click', submitForm);
 
+// Event invoked on form submit button click
 function submitForm(e) {
     e.preventDefault();
 
+    // Reg Expressions for Name, Username, Email, Password and Retype Password
     let nameRegExp = /^[A-Za-z]{3,100}$/;
     let unameRegExp = /^([A-Za-z_]{2,}\.?\w+){1,}$/;
     let emailRegExp =
         /^([\w\.]+)@([A-Za-z0-9]+).([a-z]+)(.[a-z]+)?$/;
     let psdRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9]{8,20}$/;
     
-        
+    // Check for name empty input
     if (name.value === '') {
         showError(name, 'Name cannot be empty');
     }
     else {
         
+        // Check for length and pattern match
         if (name.value.length < 3) {
             showError(name,'Name should have at least 3 letters');
         }
@@ -49,10 +45,13 @@ function submitForm(e) {
         }
     }
     
+    // Check for empty username input
     if (uname.value === '') {
         showError(uname, 'Username cannot be empty');
     }
     else {
+        // Check for username length and pattern match
+
         if (uname.value.length < 3 || uname.value.length > 50) {
             showError(uname,'Username must have 3-50 characters');
         }
@@ -70,10 +69,13 @@ function submitForm(e) {
         }
     }
     
+    // Check for empty email input
     if (email.value === '') {
         showError(email, 'Email cannot be empty');
     }
     else {
+        // Check for pattern match
+
         if (!emailRegExp.test(email.value)) {
             showError(email,'Invalid Email ID');
         }
@@ -82,10 +84,13 @@ function submitForm(e) {
         }
     }
     
+    // Check for empty password input
     if (psd.value === '') {
         showError(psd, 'Password cannot be empty');
     }
     else {
+        // Check for length and pattern match
+
         if (psd.value.length < 8 || psd.value.length > 20) {
             showError(psd,'Password must have 8-20 characters');
         }
@@ -97,18 +102,24 @@ function submitForm(e) {
         }
     }
     
+    // Check for empty retype password input
     if (retypePsd.value === '') {
         showError(retypePsd, 'Retype Password cannot be empty');
     }
-    else if (psd.value!= retypePsd.value) {
-        showError(retypePsd, 'Retype Password should match Password');
-    }
     else {
-        showSuccess(retypePsd);
+        // Check for password and retype password match
+        if (psd.value!= retypePsd.value) {
+            showError(retypePsd, 'Retype Password should match Password');
+        }
+        else {
+            showSuccess(retypePsd);
+        }
     }
-    
+
 }
 
+
+// Function to show error message
 function showError(input, message) {
     const formGroup = input.parentElement;
     formGroup.classList.remove('show-success');
@@ -118,6 +129,7 @@ function showError(input, message) {
     errorDiv.innerHTML = message;
 }
 
+// Function to remove error message and show success border
 function showSuccess(input) {
     const formGroup = input.parentElement;
     formGroup.classList.add('show-success');
